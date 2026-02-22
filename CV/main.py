19dac622fec47f17258a2c7a68520b4d6bfcc8bc
Naijei <nj277@cornell.edu>
2026-02-22 02:07:52 -0500

    full sce

--- CV/main.py
+++ CV/main.py
@@ -148,10 +148,10 @@ def make_config(args: argparse.Namespace) -> Config:
             "CRASH": 4,
         },
         drum_name_to_highlight_r_m={
-            "SNARE": 0.14,
-            "BASS": 0.20,
-            "HIHAT": 0.17,
-            "CRASH": 0.16,
+            "SNARE": 0.7,
+            "BASS": 0.10,
+            "HIHAT": 0.8,
+            "CRASH": 0.8,
         },
     )
 
